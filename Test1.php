a51d67dede4c35947d43595c2d09ef837956cc83
kimchrea <kimchreakm@gmail.com>
2017-12-06 15:32:41 +0700

    OK

---
+++ Test1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+</head>
+
+<body>
+<?PHP
+echo "Hello NPIC 1st class"; // echo គឺជា Code សម្រាប់បង្ហាញចេញ
+echo "<br/>";
+print "123456";
+?>
+
+</body>
+</html>
